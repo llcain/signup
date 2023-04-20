77e641bbd5aec1e293d16d86d8922e8f8a7afa76
llcain <llcain2012@gmail.com>
2023-04-19 21:15:10 -0500

    Changed Learn React to my name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LaQuisha Cain
         </a>
       </header>
     </div>
